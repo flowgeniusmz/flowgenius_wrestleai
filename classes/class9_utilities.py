237cb935b302fcafd21884db17859bb9f73f893e
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-06-05 04:16:31 +0000

    afd

--- classes/class9_utilities.py
+++ classes/class9_utilities.py
@@ -2,7 +2,8 @@ import streamlit as st
 import base64
 
 class Utilities:
-    def encode_image(self, image_path: str):
+    @staticmethod
+    def encode_image( image_path: str):
         with open(image_path, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read()).decode()
             return encoded_string
