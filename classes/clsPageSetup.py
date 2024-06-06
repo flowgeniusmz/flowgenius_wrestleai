05d7bec11b75fcea16c6e1866078737074376f34
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-06-06 12:06:49 -0500

    Update clsPageSetup.py

--- classes/clsPageSetup.py
+++ classes/clsPageSetup.py
@@ -104,4 +104,81 @@ class PageUtilities:
             content = st.secrets.styles.style_header1_green.format(text)
         header = st.markdown(body=content, unsafe_allow_html=True)
         return header
-        
+    
+    @staticmethod
+    def get_popover_menu(pagenumber: int):
+        menu = st.popover(label="Menu")
+        page_count = st.secrets.pages.count
+        with menu:
+            for i in range(page_count):
+                st.page_link(page=st.secrets.pages.paths[i], label=st.secrets.pages.subtitles[i], disabled=(pagenumber == i))
+        return menu
+    
+    @staticmethod
+    def manual_title(title: str, subtitle: str, div: bool=False):
+        style = st.secrets.styles.style_title1.format(title, subtitle)
+        title_display = st.markdown(body=style, unsafe_allow_html=True)
+        if div:
+            st.divider()
+        return title_display
+    
+    @staticmethod
+    def auto_title(pagenumber: int, div: bool=False):
+        title = st.secrets.pages.titles[pagenumber]
+        subtitle = st.secrets.pages.subtitles[pagenumber]
+        style = st.secrets.styles.style_title1.format(title, subtitle)
+        title_display = st.markdown(body=style, unsafe_allow_html=True)
+        if div:
+            st.divider()
+        return title_display
+    
+    @staticmethod
+    def get_userstate_menu():
+        menu = st.popover(label="Try It Now")
+        with menu:
+            newuser_button = st.button(label="Sign Up", type="primary", use_container_width=True)
+            existuser_button = st.button(label="Login", type="primary", use_container_width=True)
+            if newuser_button:
+                st.session_state.userstate = 2
+                st.rerun()
+            if existuser_button:
+                st.session_state.userstate = 4
+                st.rerun()
+
+    @staticmethod
+    def get_header_titlewithmenu(title: str, subtitle:str, menu_type: Literal["user", "nav"], pagenumber: int, div: bool=True):
+        header_container = st.container(border=False)
+        with header_container:
+            header_subcontainer = st.container(border=False)
+            with header_subcontainer:
+                header_columns = st.columns([10, 2])
+                with header_columns[0]:
+                    title = PageUtilities.manual_title(title=title, subtitle=subtitle)
+                with header_columns[1]:
+                    if menu_type == "nav":
+                        menu = PageUtilities.get_popover_menu(pagenumber=pagenumber)
+                    elif menu_type == "user":
+                        menu = PageUtilities.get_userstate_menu()  
+            if div:
+                st.divider()
+        return header_container
+    
+    @staticmethod
+    def get_material_icon(icon_name: str):
+        icon_style = st.secrets.styles.style_icon1_material
+        icon = icon_style.format(icon_name)
+        return icon
+
+    @staticmethod
+    def get_styled_container1(height: int=None, border: bool=False):
+        styleouter = st.secrets.styles.style_container2
+        styleinner = st.secrets.styles.style_container1
+        outer = sc(key="outer", css_styles=styleouter)
+        with outer:
+            inner = sc(key="inner", css_styles=styleinner)
+            with inner:
+                if height is not None:
+                    container = st.container(height=height, border=border)
+                else:
+                    container = st.container(border=border)
+        return container
