d254ad71ca2a93679adb539f8d23aab26040a7bd
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-06-06 09:43:40 -0500

    Delete classes directory

--- classes/class9_utilities.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import base64
-
-class Utilities:
-    @staticmethod
-    def encode_image( image_path: str):
-        with open(image_path, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode()
-            return encoded_string
